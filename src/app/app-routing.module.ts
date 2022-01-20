56295ea614beb5850026b636b2262094238ba83a
Kaoschuks <kaosemeka@yahoo.com>
2022-01-20 15:09:13 +0100

    initial commit with layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './components/layouts/auth-layout/auth-layout.component';
+import { SidebarLayoutComponent } from './components/layouts/sidebar-layout/sidebar-layout.component';
+import { AuthGuardsService } from './guards/app.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', loadChildren: () => import('./pages/apps/apps.module').then( m => m.AppsModule), component: SidebarLayoutComponent, 
+    // canLoad: [AuthGuardsService] 
+  },
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthModule), component: AuthLayoutComponent },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
