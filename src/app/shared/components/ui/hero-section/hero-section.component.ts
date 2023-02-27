373a21f158fdfbddd84352ed56222704343ede46
Lewis U Uzoma <52278828+lewisuzoma@users.noreply.github.com>
2023-02-27 12:59:03 +0100

    some changes

--- src/app/shared/components/ui/hero-section/hero-section.component.ts
+++ src/app/shared/components/ui/hero-section/hero-section.component.ts
@@ -1,19 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeroSectionComponent {
 
   @Output() public formEvent = new EventEmitter<any>();
   launchForm: FormGroup;
 
+  @ViewChild('swiper') swiper: SwiperComponent;
+  // @ViewChild('bgImg') bgImg: IonImg;
+
   constructor(
     private formBuilder: FormBuilder
     ) { }
+
+  sliderConfig: SwiperOptions = {
+    slidesPerView: 1,
+    navigation: true,
+  }
+
+  slideImages: any = [
+    'assets/images/hero1.png',
+    'assets/images/hero2.png',
+    'assets/images/hero3.png'
+  ];
 
   ngOnInit() {
     this.launchForm = this.formBuilder.group({
