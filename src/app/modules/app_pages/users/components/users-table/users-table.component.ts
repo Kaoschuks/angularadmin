c49a113c169a3706d071d0a3d2f54755cafde727
Favour Chukwuemeka <kaosemeka@yahoo.com>
2024-05-20 12:17:33 +0100

    updated code

--- src/app/modules/app_pages/users/components/users-table/users-table.component.ts
+++ src/app/modules/app_pages/users/components/users-table/users-table.component.ts
@@ -16,6 +16,26 @@ export class UsersTableComponent implements AfterViewInit {
       colReorder: true,
       paginate: true,
       ordering: true,
+      autoWidth: !1,
+      responsive: !0,
+      lengthMenu: [[10, 15, 30, 45, -1], ["10 Rows", "15 Rows", "30 Rows", "45 Rows", "Everything"]],
+      language: {
+          searchPlaceholder: "Search for records..."
+      },
+      sDom: '<"dataTables__top"lfB>rt<"dataTables__bottom"ip><"clear">',
+      buttons: [{
+          extend: "excelHtml5",
+          title: "Export Data"
+      }, {
+          extend: "csvHtml5",
+          title: "Export Data"
+      }, {
+          extend: "print",
+          title: "Material Admin"
+      }],
+      initComplete: (a: any, b: any) => {
+          $(this).closest(".dataTables_wrapper").find(".dataTables__top").prepend('<div class="dataTables_buttons hidden-sm-down actions"><span class="actions__item zmdi zmdi-print" data-table-action="print" /><span class="actions__item zmdi zmdi-fullscreen" data-table-action="fullscreen" /><div class="dropdown actions__item"><i data-toggle="dropdown" class="zmdi zmdi-download" /><ul class="dropdown-menu dropdown-menu-right"><a href="" class="dropdown-item" data-table-action="excel">Excel (.xlsx)</a><a href="" class="dropdown-item" data-table-action="csv">CSV (.csv)</a></ul></div></div>')
+      }
     });
     new DataTable('#accounttable');
   }
